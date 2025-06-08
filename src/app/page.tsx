882106d7dfd4594fcf9b2409968bced27f393a9b
Yurii <oniks9821@gmail.com>
2025-06-08 15:30:29 +0300

    refactor(app): add link companies and dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import Link from 'next/link';
+import React from 'react';
+
 export default function Home() {
   return (
-    <main>
+    <main className="flex flex-col items-center justify-center min-h-screen text-center gap-2">
       <h1 className="text-xl">Home page</h1>
+      <Link href="/companies" className="text-blue-500">
+        Companies
+      </Link>
+      <Link href="/dashboard" className="text-blue-500">
+        Dashboard
+      </Link>
     </main>
   );
 }
